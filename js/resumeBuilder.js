f18bc039beafe64172f07e84ab205a64a7a57a2e
Alphard <3073246911@qq.com>
2017-09-25 16:00:24 +0800

    Add work

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,88 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+/*
+var formattedName = HTMLheaderName.replace("%data%", "Alphard");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+var bio = {
+	"name" = "Alphard",
+	"role" = "Web Developer",
+	"contact info" = "XXX@qq.com",
+	"picture URL" = "images/test.jpg",
+	"welcome message" = "Welcome Programer!",
+	"skills" = "Program",
+};
+//$('#header').prepend(formattedRole);
+//$('#header').prepend(formattedName);
+$('#header').append(HTMLheaderName.replace("%data%", bio["name"]));
+*/
+var work = {"jobs" : [
+	{"employer" : "CCB",
+	"title" : "bank teller",
+	"location" : "Zhongzhou",
+	"dates" : "2014-07-10",
+	"description" : "Work in the bank teller reception"},
+	{"employer" : "CCB",
+	"title" : "technician",
+	"location" : "Anyangfenhang",
+	"dates" : "2016-08-01",
+	"description" : "Technical support work"}
+]}
+
+var projects = {"projects" : [
+	{"title" : "",
+	"dates" : "",
+	"description" : "",
+	"img" : []}
+]}
+
+var bio = {
+	"name" : "Alphard",
+	"role" : "",
+	"welcomMessage" : "",
+	"biopic" : "",
+	"contacts" : {
+		"mobile" : 1234567890,
+		"email" : "",
+		"github" : "",
+		"twitter" : "",
+		"location" : ""
+	},
+	"skill" : ["HTML", "CSS", "JavaScript"]
+}
+
+var education = {"schools" : [
+	{"name" : "",
+	"location" : "",
+	"degree" : "",
+	"dates" : "",
+	"url" : "",
+	"majors" : [],
+	"onlineCourses" : [
+		{"title" : "",
+		"schools" : "",
+		"dates" : "",
+		"url" : ""}
+	]}
+]}
+
+if(bio.skill.length > 0) {
+	$("#header").append(HTMLheaderName.replace("%data%", bio.name));
+	$("#header").append(HTMLskillsStart);
+
+	var formattedSkill = HTMLskills.replace("%data%", bio.skill[0]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skill[1]);
+	$("#skills").append(formattedSkill);
+	formattedSkill = HTMLskills.replace("%data%", bio.skill[2]);
+	$("#skills").append(formattedSkill);
+}
+
+for(job in work.jobs){
+	$("#workExperience").append(HTMLworkStart);
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+	$(".work-entry:last").append(formattedTitle);
+}
